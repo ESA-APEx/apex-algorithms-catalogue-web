f1e33089142c65f1552fbdf52959c30b290f68d1
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2025-03-11 15:21:33 +0100

    fix(MKTP-503): log error when failing to connect to db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -9,35 +9,44 @@ export const initDb = async () => {
         return db;
     }
 
-    if (dbInitPromise) {
+    try {
+        if (dbInitPromise) {
+            return await dbInitPromise;
+        }
+
+        dbInitPromise = (async () => {
+            console.log('Initializing DB');
+            const dbInstance = await Database.create(':memory:');
+
+            console.log('Setting DuckDB home directory');
+            await dbInstance.run(`SET home_directory='./tmp';`);
+
+            console.log('Installing httpfs');
+            await dbInstance.run(`INSTALL httpfs;`);
+            await dbInstance.run(`LOAD httpfs;`);
+
+            console.log('Database initialized');
+
+            db = dbInstance;
+            return dbInstance;
+        })();
+
         return await dbInitPromise;
+    } catch (error) {
+        console.log('Cannot connect to DB: ' + String(error));
+        throw error;
     }
-
-    dbInitPromise = (async () => {
-        console.log('Initializing DB');
-        const dbInstance = await Database.create(':memory:');
-
-        console.log('Setting DuckDB home directory');
-        await dbInstance.run(`SET home_directory='./tmp';`);
-
-        console.log('Installing httpfs');
-        await dbInstance.run(`INSTALL httpfs;`);
-        await dbInstance.run(`LOAD httpfs;`);
-
-        console.log('Database initialized');
-
-        db = dbInstance;
-        return dbInstance;
-    })();
-
-    return await dbInitPromise;
-
 }
 
 export const executeQuery = async (query: string): Promise<any[]> => {
-    if (!db) {
-        db = await initDb();
+    try {
+        if (!db) {
+            db = await initDb();
+        }
+        const connection = await db.connect();
+        return (await connection.all(query)) as any[];
+    } catch (error) {
+        console.log('Cannot execute query: ' + String(error));
+        throw error;
     }
-    const connection = await db.connect();
-    return (await connection.all(query)) as any[];
 }
