23533a3dfcffd27d32d6322b98a39b9f1314afb8
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2025-04-15 16:03:40 +0200

    fix(e2e): trigger API call before running the test for faster execution

---
+++ playwright-global-setup.ts
@@ -0,0 +1,13 @@
+import type { FullConfig } from '@playwright/test';
+
+const globalSetup = async (_config: FullConfig): Promise<void> => {
+    console.log('Manually initialize cache by triggering API call, making the test faster...')
+    try {
+        await fetch('http://localhost:4321/api/services/benchmarks.json');
+        console.log('Initializing cache done ✅.')
+    } catch (error) {
+        throw error;
+    }
+}
+
+export default globalSetup;
