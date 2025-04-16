72cbfbde2f9852e6808f0d2c63b5f420af2fca3f
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2025-04-16 10:04:32 +0200

    chore(e2e): debug global setup

--- playwright-global-setup.ts
+++ playwright-global-setup.ts
@@ -1,11 +1,12 @@
 import type { FullConfig } from '@playwright/test';
 
 const globalSetup = async (_config: FullConfig): Promise<void> => {
-    console.log('Manually initialize cache by triggering API call, making the test faster...')
+    console.log('Manually initialize cache by triggering API call, making the test faster...');
     try {
         await fetch('http://localhost:4321/api/services/benchmarks.json');
-        console.log('Initializing cache done ✅.')
+        console.log('Initializing cache done ✅.');
     } catch (error) {
+        console.error(error);
         throw error;
     }
 }
