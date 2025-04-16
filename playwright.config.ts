4fb92bbad247ca149937d8685ed1c7aa07a7001d
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2025-04-16 09:57:51 +0200

    chore(e2e): run server on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,6 @@ export default defineConfig({
     command: 'npm run preview',
     url: 'http://localhost:4321/',
     timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
