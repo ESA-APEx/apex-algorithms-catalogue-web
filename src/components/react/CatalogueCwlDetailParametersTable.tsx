0dd602b45d226bc3f11ad8be4f9ed13d1c85351e
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2025-05-14 10:27:51 +0200

    fix(e2e): create e2e test for service label and parameters table

--- src/components/react/CatalogueCwlDetailParametersTable.tsx
+++ src/components/react/CatalogueCwlDetailParametersTable.tsx
@@ -47,41 +47,39 @@ export const CatalogueCwlDetailParametersTable = ({ cwlUrl }: CatalogueCwlDetail
             status === 'loading' ? <p>Fetching parameters...</p> 
             : (
                 details && (
-                    <>
-                        <Table className="bg-white bg-opacity-5 rounded-lg">
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead className="w-2/3">Parameter</TableHead>
-                                    <TableHead>Type</TableHead> 
-                                    <TableHead>Default</TableHead>
+                    <Table className="bg-white bg-opacity-5 rounded-lg" data-testid="parameters-table">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-2/3">Parameter</TableHead>
+                                <TableHead>Type</TableHead> 
+                                <TableHead>Default</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {details.parameters.map((parameter) => (
+                                <TableRow key={parameter.name}>
+                                    <TableCell>
+                                        <p className="mb-1">{parameter.name} {!parameter?.optional && <span>(required)</span>}</p>
+                                        <p className="text-sm text-gray-300">
+                                            {parameter.description}
+                                        </p>
+                                    </TableCell>
+                                    <TableCell>
+                                        <div className="flex items-center gap-1">
+                                            <span>{parameter.schema}</span>
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        {
+                                            parameter.hasOwnProperty('default') && typeof parameter.default === 'object' ? 
+                                            JSON.stringify(parameter.default) : 
+                                            parameter.default
+                                        }
+                                    </TableCell>
                                 </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {details.parameters.map((parameter) => (
-                                    <TableRow key={parameter.name}>
-                                        <TableCell>
-                                            <p className="mb-1">{parameter.name} {!parameter?.optional && <span>(required)</span>}</p>
-                                            <p className="text-sm text-gray-300">
-                                                {parameter.description}
-                                            </p>
-                                        </TableCell>
-                                        <TableCell>
-                                            <div className="flex items-center gap-1">
-                                                <span>{parameter.schema}</span>
-                                            </div>
-                                        </TableCell>
-                                        <TableCell>
-                                            {
-                                                parameter.hasOwnProperty('default') && typeof parameter.default === 'object' ? 
-                                                JSON.stringify(parameter.default) : 
-                                                parameter.default
-                                            }
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </>
+                            ))}
+                        </TableBody>
+                    </Table>
                 )
             )
         ) : null;
--- src/components/react/InfoPopover.tsx
+++ src/components/react/InfoPopover.tsx
@@ -8,11 +8,11 @@ export interface InfoPopoverProps {
 export const InfoPopover = ({ message }: InfoPopoverProps) => {
 
     return (
-        <Popover>
+        <Popover data-testid="info-popover">
             <PopoverTrigger>
                 <Info className="w-4 h-4 text-white rounded-full hover:bg-brand-teal-50/20 cursor-pointer" />
             </PopoverTrigger>
-            <PopoverContent className="relative">
+            <PopoverContent className="relative" data-testid="info-popover-content">
                 <article>
                     <p className="text-sm">
                         {message}
