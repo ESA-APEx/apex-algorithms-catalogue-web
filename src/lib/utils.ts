4282bc0235bdf58209fdd3f50d3fc54ed124e0bc
bramjanssen <brm.janssen@gmail.com>
2025-10-20 11:47:07 +0200

    fix: removed typo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { BASE_PATH } from "@/config";
-import { a } from "vitest/dist/chunks/suite.qtkXWc6R.js";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
