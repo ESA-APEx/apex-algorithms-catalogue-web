46c2ad1eddf92ba34e667c34355e77ff3516ff21
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2025-12-02 14:18:43 +0100

    feat: setup keycloak auth

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 declare module "chartjs-adapter-date-fns";
+
+declare namespace App {
+  interface Locals {
+    user?: {
+      username?: string;
+      email?: string | null;
+      roles?: string[];
+    };
+  }
+}
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,37 @@
 import { Database } from "duckdb-async";
+import type { DefaultSession } from "@auth/core/types";
 
 declare global {
   var cachedUrls: string[];
   var cachedUrlsExpireTime: Date;
   var db: Database | null;
   var dbInitPromise: Promise<Database> | null;
+}
+
+declare module "@auth/core/types" {
+  interface Session {
+    user: {
+      username?: string;
+      roles?: string[];
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    username?: string;
+    roles?: string[];
+  }
+
+  interface JWT {
+    username?: string;
+    roles?: string[];
+  }
+
+  interface Profile {
+    preferred_username?: string;
+    realm_access?: {
+      roles: string[];
+    };
+  }
 }
 
 export {};
