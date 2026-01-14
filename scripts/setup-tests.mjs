ef1117ab5e019bbf09078511c9f32eb3e2ce5711
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2026-01-14 15:48:15 +0100

    chore(e2e): use fixtures for faster benchmark related tests

--- scripts/setup-tests.mjs
+++ scripts/setup-tests.mjs
@@ -3,7 +3,6 @@ import path from "path";
 
 modifySarCoinJson();
 createPrivateAlgorithm();
-copyAclMapping();
 
 function modifySarCoinJson() {
   console.log("Modifying sar_coin.json...");
@@ -41,19 +40,4 @@ function createPrivateAlgorithm() {
 
   fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), "utf-8");
   console.log("Creation of private algorithm complete ✅.");
-}
-
-function copyAclMapping() {
-  console.log("Copying ACL mapping for tests...");
-
-  const sourcePath = path.resolve("./src/acl-mapping.json");
-  const targetPath = path.resolve("./dist/server/acl-mapping.json");
-  const distServerPath = path.dirname(targetPath);
-
-  if (!fs.existsSync(distServerPath)) {
-    fs.mkdirSync(distServerPath, { recursive: true });
-  }
-  fs.copyFileSync(sourcePath, targetPath);
-
-  console.log("Copying ACL mapping completed ✅.");
 }
