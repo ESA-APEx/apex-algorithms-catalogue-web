cdde2a0561321060f5a024f5b86a7e2655178783
Bram Janssen <bram.janssen@vito.be>
2026-02-03 11:29:07 +0100

    test: removed unused import

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -1,5 +1,5 @@
 import base from '@playwright/test';
-import { read, readFileSync } from 'node:fs';
+import { readFileSync } from 'node:fs';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
