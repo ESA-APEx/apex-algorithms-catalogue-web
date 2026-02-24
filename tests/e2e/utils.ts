e3317df59fe43c67d786648924313c52d73c14ea
Rahmanda Wibowo <rahmanda.wibowo@vito.be>
2026-02-24 11:25:07 +0100

    fix(test): mock max_ndvi_composite benchmark detail request

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -1,35 +1,41 @@
-import base from '@playwright/test';
-import { readFileSync } from 'node:fs';
-import { join, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-
+import base from "@playwright/test";
+import { readFileSync } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const getFixturesDir = () => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    return join(__dirname, '../fixtures');
-}
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  return join(__dirname, "../fixtures");
+};
 const readFixture = (filename: string) => {
-    return JSON.parse(
-        readFileSync(join(getFixturesDir(), filename), 'utf-8')
-    );
+  return JSON.parse(readFileSync(join(getFixturesDir(), filename), "utf-8"));
+};
 
-}
-
-
-const defaultBenchmark = readFixture('benchmarks.json');
+const defaultBenchmark = readFixture("benchmarks.json");
 
 export const test = base.extend<{ forEachTest: void }>({
-    forEachTest: [async ({ page }, use) => {
-        await page.route('api/services/benchmarks.json', route => {
-            return route.fulfill({
-                json: defaultBenchmark
-            })
+  forEachTest: [
+    async ({ page }, use) => {
+      await page.route("api/services/benchmarks.json", (route) => {
+        return route.fulfill({
+          json: defaultBenchmark,
         });
+      });
 
-        await use();
-    }, { auto: true }],  // automatically starts for every test.
+      await page.route(
+        "api/services/max_ndvi_composite/benchmarks.json",
+        (route) => {
+          return route.fulfill({
+            json: readFixture("/benchmarks/max_ndvi_composite.json"),
+          });
+        },
+      );
+
+      await use();
+    },
+    { auto: true },
+  ], // automatically starts for every test.
 });
 
-export { expect } from '@playwright/test';
+export { expect } from "@playwright/test";
